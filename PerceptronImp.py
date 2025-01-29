c897757497ab1fab3eb1efbcd8d84f70e84143bf
Daniel Neagu <danielgneagu@gmail.com>
2025-01-29 14:57:47 -0500

    initialized w

--- PerceptronImp.py
+++ PerceptronImp.py
@@ -5,7 +5,10 @@ from sklearn.metrics import confusion_matrix
 
 def fit_perceptron(X_train, y_train):
     #Add implementation here 
-
+    #initializes the set w to an array of 0s to the same dimensions as an input
+    w = np.zeros(np.shape(X_train[0]))
+    
+"""
 def errorPer(X_train,y_train,w):
     #Add implementation here 
 
@@ -42,4 +45,10 @@ def test_Part1():
     print("Confusion Matrix from Part 1b is:",sciKit)
     
 
-test_Part1()
+test_Part1()"""
+
+if __name__ == "__main__":
+    from sklearn.datasets import load_iris
+    X_train, y_train = load_iris(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(X_train[50:],y_train[50:],test_size=0.2)
+    fit_perceptron(X_train,y_train)
